71a19833ef73d80bc693f7a955b3c3f372b90ef8
vijay pulavarthi <spulavar@iu.edu>
2024-01-31 01:18:05 -0500

    bdhgds

--- close_position.php
+++ close_position.php
@@ -2,10 +2,10 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include 'dbconnection.php';
     // Get form data
-    $position_id = $_POST["position_id"];
+    $id = $_POST["id"];
 
     // Update the status to 'closed'
-    $sql = "UPDATE jobposted SET status = 'close' WHERE id = $position_id";
+    $sql = "UPDATE jobs SET position = 'close' WHERE id = $id";
     $conn->query($sql);
 
     // Redirect to the view_positions page or another page
