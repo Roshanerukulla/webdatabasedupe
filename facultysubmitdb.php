c02c98966e0f2dd8800d39ee16f2403de9629ad3
Roshanerukulla <roshan8019924268@gmail.com>
2024-01-30 23:56:15 -0500

    faculty updated

--- facultysubmitdb.php
+++ facultysubmitdb.php
@@ -9,9 +9,21 @@ $jobdes= $_POST["jobdes"];
 $jobreq=$_POST["jobreq"];
 $jobpay=$_POST["jobpay"];
 $jobskills=$_POST["jobskills"];
-$info=$_POST["info"]
+$info=$_POST["info"];
+$position=$_POST["position"];
 
+ // Insert data into the database
+ $sql = "INSERT INTO jobposted (jobtitle, jobdes, jobreq, jobpay, jobskills, info, position) VALUES ('$jobtitle, $jobdes, $jobreq, $jobpay, $jobskills, $info, $position)";
 
+ if (mysqli_query($conn, $sql)) {
+     echo "New record created successfully";
+ } else {
+     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+ }
+ 
+ mysqli_close($conn);
 }
+
+
 
 ?>
